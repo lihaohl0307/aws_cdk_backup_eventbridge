9b0057ee7f2e166ee6da563081ad042f62daa29c
EC2 Default User <ec2-user@ip-172-31-19-188.ec2.internal>
2025-11-01 00:09:06 +0000

    Initial commit

---
+++ lib/cs6620_midterm-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Cs6620MidtermStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Cs6620MidtermQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
